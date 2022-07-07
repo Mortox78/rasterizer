e4619d65b7709250d6bbdebe6025f3486070c222
Mortox78 <steeven_yugo@hotmail.fr>
2022-07-07 17:55:50 +0200

    CLean fichier temporaires

--- src/Scene.d
+++
@@ -1,8 +0,0 @@
-src/Scene.o: src/Scene.cpp include/Scene.h include/Entity.h \
- include/Mesh.h include/Vertex.h include/matrix.h \
- include/inlVec/vector.hpp include/inlVec/vector2D.inl \
- include/inlVec/vector.hpp include/inlVec/vector3D.inl \
- include/inlVec/vector4D.inl include/submatrix.inl include/matrix.inl \
- include/matrix4.inl include/Color.h include/Color.inl include/Vertex.inl \
- include/Texture.hpp include/vector.hpp include/Light.hpp \
- include/Entity.h
